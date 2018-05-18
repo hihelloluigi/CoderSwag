de3272be63bcb3a4f186edbe5e677c03864a6624
Unknown <luigiaiello97@gmail.com>
2018-05-18 17:32:58 +0200

    Added product view adapter

---
+++ app/src/main/java/com/example/luigi/coderswag/Adapters/ProductsAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.luigi.coderswag.Adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.luigi.coderswag.Model.Product
+import com.example.luigi.coderswag.R
+import kotlinx.android.synthetic.main.product_list_item.view.*
+
+class ProductsAdapter(val context: Context, val products: List<Product>): RecyclerView.Adapter<ProductsAdapter.ProductHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.product_list_item, parent, false)
+        return ProductHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return products.count()
+    }
+
+    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
+        holder.bindProduct(products[position], context)
+    }
+
+    inner class ProductHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        private val productImage = itemView?.findViewById<ImageView>(R.id.productImage)
+        private val productName = itemView?.findViewById<TextView>(R.id.productName)
+        private val productPrice = itemView?.findViewById<TextView>(R.id.productPrice)
+
+        fun bindProduct(product: Product, context: Context) {
+            val resourceId = context.resources.getIdentifier(product.image, "drawable", context.packageName)
+            productImage?.setImageResource(resourceId)
+            productName?.text = product.title
+            productPrice?.text = product.price
+        }
+    }
+}
