5cb8d4a7079499d6a21e791580818bd388c2a3bb
Unknown <luigiaiello97@gmail.com>
2018-05-18 12:02:17 +0200

    Added Model View Controller

--- app/src/main/java/com/example/luigi/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/luigi/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.luigi.coderswag
+package com.example.luigi.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.luigi.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
