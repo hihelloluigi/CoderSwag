9ef18e107869842a989bdc217863dc9f6947b03e
Unknown <luigiaiello97@gmail.com>
2018-05-18 16:16:22 +0200

    Added recycler view

--- app/src/main/java/com/example/luigi/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/luigi/coderswag/Controller/MainActivity.kt
@@ -2,26 +2,52 @@ package com.example.luigi.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
+import android.widget.Toast
 import com.example.luigi.coderswag.Adapters.CategoryAdapter
+import com.example.luigi.coderswag.Adapters.CategoryRecycleAdapter
 import com.example.luigi.coderswag.R
 import com.example.luigi.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    //Array Adapter
+    //lateinit var adapter: CategoryAdapter
+
+    //Array Adapter
     //lateinit var adapter: ArrayAdapter<Category>
+
+    //Recycler Adapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Array Adapter
         //adapter = ArrayAdapter(this,
                 //android.R.layout.simple_list_item_1,
                 //DataService.categories)
-        adapter = CategoryAdapter(this,
-                DataService.categories)
+
+        //Custom Adapter
+        //adapter = CategoryAdapter(this,
+               // DataService.categories)
+
+        //Recycler Adapter
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        //You can do this optimization if knows that cell height never change
+        categoryListView.setHasFixedSize(true)
 
         categoryListView.adapter = adapter
+
+        //Did Select View
+        //categoryListView.setOnItemClickListener { parent, view, position, id ->
+         //   val category = DataService.categories[position]
+         //   Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
+       // }
     }
 }
